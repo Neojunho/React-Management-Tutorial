037187d335e9f5a6037c59c600256289137ab18f
Seojunho <neojunho@gmail.com>
2020-05-27 10:46:53 +0900

    Add Get Customers Rest Api Module

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import { withStyles } from '@material-ui/core/styles';
 const styles = theme => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     overflowX: "auto"
   },
   table: {
@@ -20,55 +20,45 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-  {
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name':'홍길동',
-  'birthday':'961222',
-  'gender':'남자',
-  'job':'대학생'
-},
-{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name':'이슬기',
-  'birthday':'961223',
-  'gender':'여자',
-  'job':'IT인'
-},
-{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name':'하유빈',
-  'birthday':'961224',
-  'gender':'남자',
-  'job':'문과인'
-},
-]
 
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+  // `theme.spacing.unit * y` with `theme.spacing(y)`
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return (
       <Paper className={classes.root}>
-        <div>
           <Table className={classes.table}>
             <TableHead>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
             </TableHead>
             <TableBody>
-              {
-                customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> )})
-              }
+              {this.state.customers ? this.state.customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);
+                }) : null}
             </TableBody>
-          </Table>                                              
-        </div>
+          </Table>
       </Paper>
     )
   }
